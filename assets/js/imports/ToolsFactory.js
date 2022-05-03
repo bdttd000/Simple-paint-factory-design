c77ac8ac1e633d37729389f07250903fbb09a752
bdttd000 <44643460+bdttd000@users.noreply.github.com>
2022-05-04 00:21:29 +0200

    Add files via upload

---
+++ assets/js/imports/ToolsFactory.js
@@ -0,0 +1,22 @@
+import { Brush } from "./tools/Brush.js";
+
+export class ToolsFactory {
+
+    constructor() {
+        this.brush = new Brush(10, 'blue');
+        // this.pencil = new Pencil(1, 'red');
+        // this.shape = new Shape(20, 'green');
+    }
+
+    getTool(tool) {
+        switch(tool) {
+            case 'brush':
+                return this.brush;
+            case 'pencil':
+                return this.pencil;
+            case 'shape':
+                return this.shape;
+        }
+    }
+
+}
